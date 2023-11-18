34c4c59fcf0c3f668058d12b8bd23ae5c340ac66
minamar <mina.marmpena@itml.gr>
2023-11-18 19:08:53 +0200

    added config

---
+++ src/configs/config.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Model config in json format"""
+
+CFG = {
+
+    "data": {
+        "path_normal": "data/unsw-nb15/short_attack_normal/normal_short.csv",
+        "path_anomaly": "data/unsw-nb15/short_attack_normal/attack_short.csv",
+        "verification_set": "data/verification/UNSW-NB15_1.csv",
+        "ground_truth_cols": ['label'],  # a list with names of columns or None
+        "features": ["dur", "proto", "service", "state", "spkts", "dpkts", "sbytes", "dbytes", "sttl", "dttl",
+                     "sload", "dload", "sloss", "dloss", "sinpkt", "dinpkt", "sjit", "djit", "swin", "stcpb", "dtcpb",
+                     "dwin", "tcprtt", "synack", "ackdat", "smean", "dmean", "trans_depth", "response_body_len",
+                     "ct_srv_src", "ct_state_ttl", "ct_dst_ltm", "ct_src_dport_ltm", "ct_dst_sport_ltm", "ct_dst_src_ltm",
+                     "is_ftp_login", "ct_ftp_cmd", "ct_flw_http_mthd", "ct_src_ltm", "ct_srv_dst", "is_sm_ips_ports"],
+        "data_types": ["Float32", "category", "category", "category", "Int64", "Int64", "Int64", "Int64",
+                       "Int64", "Int64", "Float32", "Float32", "Int64", "Int64", "Float32", "Float32",
+                       "Float32", "Float32", "Int64", "Int64", "Int64", "Int64", "Float32", "Float32", "Float32",
+                       "Int64", "Int64", "Int64", "Int64", "Int64", "Int64", "Int64", "Int64", "Int64", "Int64",
+                       "category", "Int64", "Int64", "Int64", "Int64", "category"],
+        "n_rows": None,
+        "normal_splits_pc": [0.7, 0.1, 0.1, 0.1],
+        "anomaly_splits_pc": [0.5, 0.5]
+    },
+    "train": {
+        "hyperparams": {
+            "encoder_layers": 2,
+            "decoder_layers": 2,
+            "encoder_units": (100, 50),
+            "decoder_units": (50, 100),
+            "learning_rate": 0.0032265,
+            "dropout_rate": 0.2,
+            "batch_size": 32,
+            "epochs": 200,
+            "activation": "tanh",
+            "regularization": None
+        },
+        "train_setup": {
+            "seq_time_steps": 4,
+            "early_stopping_rounds": 5,
+            "tuning": True,
+            "tuning_max_evals": 500,
+            "hp_space": "lstm-ae-extra"
+        },
+        "anomaly_scoring": {
+            "beta": 1.0,
+            "num_thresholds": 20
+        }
+    },
+    "model": {
+        "model_name": "LSTM-AE",
+        "artifact_storage_path": "local_artifacts"
+    },
+    "mlflow_config": {
+        "enabled": True,
+        "experiment_name": "teaching-ADLM-v6",
+    },
+    "inference": {
+        "data_path": "data/verification/UNSW-NB15_1.csv",
+        "ground_truth_cols": ['label'],  # or None for new data
+        "model_path": "local_model_storage/lstmae",
+        "transformer_path": "local_model_storage/transformer.sav"
+    }
+}
